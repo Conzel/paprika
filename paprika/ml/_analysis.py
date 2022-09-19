e3dd20da800f4aa6d0cfcf0d83c45504acac0751
Szőcs Orsolya <vampirereflex@gmail.com>
2022-09-19 12:43:33 +0200

    refactor: analysis returns image paths when possible

--- paprika/ml/_analysis.py
+++ paprika/ml/_analysis.py
@@ -1,85 +1,99 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
+from typing import List
+
+
 class ClassPrediction:
     """
-    Represents a prediction of the neural network. 
+    Represents a prediction of the neural network.
 
-    label: the name of the class that the neural network predicted. 
-    score: the confidence of the prediction in percent. 
-    similar_images: list of flexible length containing images that are similar to the input image 
+    label: the name of the class that the neural network predicted.
+    score: the confidence of the prediction in percent.
+    similar_images: list of flexible length containing the full paths of images that are similar to the input image
     """
-    def __init__(self, label: str, score: float, similar_images: list[np.ndarray]):
+
+    def __init__(self, label: str, score: float, similar_images: List[str]):
         self.label = label
         self.score = score
         self.similar_images = []
 
+
 class NeuralNetworkAnalysis(ABC):
     """
-    This interface represents the pass of an image through our Neural Network. 
+    This interface represents the pass of an image through our Neural Network.
 
     The analysis is performed on the image that is passed to the constructor.
     It may be performed lazily only when the corresponding method (f.e. get_most_activated_filters)
-    are called. All results are received via class-specific methods. 
+    are called. All results are received via class-specific methods.
 
-    The interface can be implemented for different neural networks. 
+    The interface can be implemented for different neural networks.
     """
+
     @abstractmethod
     def __init__(self, img: np.ndarray):
         """Performs the analysis on the given image."""
         pass
 
     @abstractmethod
-    def get_most_activated_filters(self, layer: int, n: int) -> list[np.ndarray]:
+    def get_most_activated_filters(self, layer: str, n: int) -> List[str]:
         """
-        Returns a list of the n most activated filters in the given layer.
+        Returns a list of the full paths of the visualisations corresponding to the
+        n most activated filters in the given layer.
         """
         pass
 
     @abstractmethod
     def get_saliency_map(self) -> np.ndarray:
         """
-        Returns a saliency map of the image. The saliency map has the same dimensions 
+        Returns a saliency map of the image. The saliency map has the same dimensions
         as the input image and is a heatmap of the most important pixels in the image.
         """
         pass
 
     @abstractmethod
-    def get_class_predictions(self, n_predictions: int, n_images: int) -> list[ClassPrediction]:
+    def get_class_predictions(
+        self, n_predictions: int, n_images: int
+    ) -> List[ClassPrediction]:
         """
-        Returns a list of the n_predictions most likely classes for the image. 
+        Returns a list of the n_predictions most likely classes for the image.
         Each class contains the score (percentage), the label (name), and a list of n_images similar images.
 
-        The images are returned in descending order of likelihood. 
+        The images are returned in descending order of likelihood.
         """
         pass
+
 
 class Inceptionv1Analysis(NeuralNetworkAnalysis):
     """
     Performs image analysis using Inceptionv1.
     """
+
     def __init__(self, img: np.ndarray):
         """Performs the analysis on the given image."""
         pass
 
-    def get_most_activated_filters(self, layer: int, n: int) -> list[np.ndarray]:
+    def get_most_activated_filters(self, layer: str, n: int) -> List[str]:
         """
-        Returns a list of the n most activated filters in the given layer.
+        Returns a list of the full paths of the visualisations corresponding to the
+        n most activated filters in the given layer.
         """
         pass
 
     def get_saliency_map(self) -> np.ndarray:
         """
-        Returns a saliency map of the image. The saliency map has the same dimensions 
+        Returns a saliency map of the image. The saliency map has the same dimensions
         as the input image and is a heatmap of the most important pixels in the image.
         """
         pass
 
-    def get_class_predictions(self, n_predictions: int, n_images: int) -> list[ClassPrediction]:
+    def get_class_predictions(
+        self, n_predictions: int, n_images: int
+    ) -> List[ClassPrediction]:
         """
-        Returns a list of the n_predictions most likely classes for the image. 
+        Returns a list of the n_predictions most likely classes for the image.
         Each class contains the score (percentage), the label (name), and a list of n_images similar images.
 
-        The images are returned in descending order of likelihood. 
+        The images are returned in descending order of likelihood.
         """
         pass
