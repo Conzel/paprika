c641a2f554dabfde46d6152b8c25b0947b7c39b2
Szőcs Orsolya <vampirereflex@gmail.com>
2022-11-26 17:15:39 +0100

    add: english labels for imagenet groups and script to verify the group names

---
+++ scripts/translations-add-id-and-english.py
@@ -0,0 +1,57 @@
+import csv
+import time
+from PIL import Image
+
+
+def process_one_class(id1, id2, german_group_label):
+    # id1, id2, german_group_label, english_group_label, english_labels_list
+    print(id1)
+    # show one image of class
+    img = 1
+    shown_image = False
+    while not shown_image:
+        try:
+            image = Image.open(f"D:\imagenet\ILSVRC\Data\CLS-LOC\\train\\{id1}\\{id1}_{img}.jpeg")
+            image.show()
+            shown_image = True
+        except FileNotFoundError:
+            img += 1
+    # show all subclasses in group
+    subclasses = []
+    for translation in translations:
+        if translation[1] == german_group_label:
+            subclasses.append(translation[0])
+    print(subclasses)
+    # show list of English labels
+    for labels_list_line in labels_list_lines:
+        if id1 in labels_list_line:
+            english_labels_list = labels_list_line
+    print(english_labels_list[10:])
+
+    english_group_label = input("english group label: ")
+    grouped_classes_writer.writerow([id1, id2, german_group_label, english_group_label, english_labels_list[10:]])
+
+
+ids = range(900, 1000)
+
+with open('grouped_classes.csv', 'a', newline='', encoding='utf-8') as result_file:
+    grouped_classes_writer = csv.writer(result_file, delimiter='|')
+
+    with open('old_labels.txt') as old_labels_file:
+        old_labels_lines = old_labels_file.read().splitlines()
+
+        with open('translations.csv', newline='', encoding='utf-8') as translations_csv:
+            translations_reader = csv.reader(translations_csv, delimiter=',')
+            translations = [row for row in translations_reader][1:]
+
+            with open('..\paprika\ml\LOC_synset_mapping.txt') as labels_list_file:
+                labels_list_lines = labels_list_file.read().splitlines()
+
+                for i in ids:
+                    translation = translations[i]
+                    old_labels_line = old_labels_lines[i]
+                    print()
+                    print(translation)
+                    print(old_labels_line)
+                    process_one_class(old_labels_line.split(" ")[0], old_labels_line.split(" ")[1], translation[1])
+
---
+++ scripts/verify-grouped-classes.py
@@ -0,0 +1,59 @@
+import os
+import random
+import time
+
+import pandas
+from matplotlib import image as mpimg
+from matplotlib import pyplot as plt
+
+start_group_index = 0
+nr_images_per_subclass = 3
+sleep_seconds = 1
+
+def get_random_images_from_folder(path, nr_images):
+    files = os.listdir(path)
+    imgs = []
+    for file in files:
+        if ".JPEG" in file:
+            imgs.append(file)
+    imgs = random.sample(imgs, k=nr_images)
+    paths = []
+    for img in imgs:
+        relative_path = path + "/" + img
+        paths.append(os.path.abspath(os.path.expanduser(os.path.expandvars(relative_path))))
+    return paths
+
+
+translations_df = pandas.read_csv("grouped_classes.csv", delimiter="|")
+grouped_classes_df = translations_df.sort_values("english_group").groupby(["english_group", "german_group"]).agg(
+    list).reset_index()
+
+for index, row in grouped_classes_df.iterrows():
+    if index < start_group_index:
+        continue
+    imagenet_id = row["id_1"]
+    german_group = row["german_group"]
+    english_group = row["english_group"]
+    english_labels = row["english_labels"]
+    nr_subgroups = len(english_labels)
+
+    print()
+    print(f"{index} / {len(grouped_classes_df) - 1}")
+    print(f"{english_group}   {german_group}")
+    print()
+    for subgroup_index in range(nr_subgroups):
+        print(f"{imagenet_id[subgroup_index]}   {english_labels[subgroup_index]}")
+        images = get_random_images_from_folder(f"../imagenet/{imagenet_id[subgroup_index]}", nr_images_per_subclass)
+        figure, axes = plt.subplots(1, nr_images_per_subclass)
+        for image_index in range(nr_images_per_subclass):
+            axes[image_index].imshow(mpimg.imread(images[image_index]))
+            axes[image_index].get_xaxis().set_visible(False)
+            axes[image_index].get_yaxis().set_visible(False)
+        figure.set_dpi(200)
+        figure.tight_layout()
+        figure.show()
+        time.sleep(sleep_seconds)
+
+    print()
+    input("press enter to go to next class\n")
+
